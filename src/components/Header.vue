e4b7f33c0938168701c0ec0e721bb42a2e6a8cb8
btaccone@itba.edu.ar <bruntacc@LAPTOP-HIRJ2COG.>
2025-05-17 17:07:44 -0300

    include header

---
+++ src/components/Header.vue
@@ -0,0 +1,11 @@
+<template>
+    <v-card
+    color="#fff2ef"
+    width="100vw"
+    height="8vh"
+    class="rounded-0"
+    elevation="0"
+    style="display: flex; align-items: center; border-bottom: solid black;">
+        <v-card-title style="font-size: 1.8rem; margin-left: 1vw"><slot/></v-card-title>
+    </v-card>
+</template>

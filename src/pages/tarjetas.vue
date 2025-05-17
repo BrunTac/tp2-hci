04e30319a9914a1ae2d72cab8bfd2eee33572779
emitchellitba <emitchell@itba.edu.ar>
2025-05-17 16:50:23 -0300

    feat: added buttons to cards page

---
+++ src/pages/tarjetas.vue
@@ -0,0 +1,22 @@
+<script setup>
+
+  import Sidebar from '@/components/Sidebar.vue';
+</script>
+
+<template>
+  <Sidebar />
+  <v-card class="w-25 flex justify-center">
+    <div class="flex flex-row">
+      <v-btn class="mx-4" size="x-large">
+        <v-icon>mdi-camera-outline</v-icon>
+      </v-btn>
+      <v-btn class="mx-4" size="x-large">
+        <v-icon>mdi-pencil-outline</v-icon>
+      </v-btn>
+    </div>
+  </v-card>
+</template>
+
+<style scoped>
+
+</style>

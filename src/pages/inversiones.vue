25ce054bfd0d1baf3fc6f6d09a5b81562aa4406b
btaccone@itba.edu.ar <bruntacc@LAPTOP-HIRJ2COG.>
2025-05-27 22:30:07 -0300

    beautified home, added coming soon pages

---
+++ src/pages/inversiones.vue
@@ -0,0 +1,16 @@
+<template>
+  <Header>Inversiones</Header>
+  <Sidebar />  
+  <v-container class="fill-height">
+    <v-row class="fill-height" align="center" justify="center">
+      <v-col cols="auto">
+        <h1 style="font-size: 2.5rem; color: black; margin-bottom: 60%">Próximamente...</h1>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script setup>
+import Header from '@/components/Header.vue';
+import Sidebar from '@/components/Sidebar.vue';
+</script>

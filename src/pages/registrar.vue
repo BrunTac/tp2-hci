9a08e510262de9c744d8530a04cfc2d6edcd8a8c
btaccone@itba.edu.ar <bruntacc@LAPTOP-HIRJ2COG.>
2025-05-28 22:35:28 -0300

    fixed register

--- src/pages/registrar.vue
+++ src/pages/registrar.vue
@@ -7,96 +7,98 @@
         <h2 style="margin-top: 10vh">Registro</h2>
         <v-alert
           v-if="showAlert"
-          closable
-          style="margin-top: 1vw"
           type="error"
           variant="tonal"
+          closable
           @click:close="showAlert = false"
+          style="margin-top: 1vw"
+          width="27vw"
         >
           {{ alertMessage }}
         </v-alert>
-        <v-form ref="form" style="display: flex; flex-direction: column;" validate-on="input" @submit.prevent>
+        <v-form ref="form" validate-on="input" @submit.prevent style="display: flex; flex-direction: column;">
 
           <v-text-field
             v-model="firstName"
-            clearable
-            color="#d28d8d"
+            validate-on="input"
             label="Nombre"
+            clearable
+            variant="outlined"
+            color="#d28d8d"
+            width="27vw"
             :rules="[rules.required]"
             style="margin-top: 1.5vw;"
-            validate-on="input"
-            variant="outlined"
-            width="27vw"
           />
 
           <v-text-field
             v-model="lastName"
-            clearable
-            color="#d28d8d"
+            validate-on="input"
             label="Apellido"
+            clearable
+            variant="outlined"
+            color="#d28d8d"
+            width="27vw"
             :rules="[rules.required]"
             style="margin-top: 1vw;"
-            validate-on="input"
-            variant="outlined"
-            width="27vw"
           />
 
           <v-text-field
-            clearable
-            color="#d28d8d"
+            v-model="birthDate"
+            validate-on="input"
             label="Fecha de Nacimiento"
-            :model-value="birthDate"
+            clearable
             placeholder="DD/MM/YYYY"
-            :rules="[rules.required, rules.date]"
-            style="margin-top: 1vw; width: 27vw;"
             variant="outlined"
-            @update:model-value="formatDate"
+            color="#d28d8d"
+            width="27vw"
+            :rules="[rules.required, rules.date]"
+            style="margin-top: 1vw;"
           />
 
           <v-text-field
             v-model="email"
-            clearable
-            color="#d28d8d"
+            validate-on="input"
             label="Email"
+            clearable
+            type="email"
             placeholder="example@gmail.com"
+            variant="outlined"
+            color="#d28d8d"
+            width="27vw"
             :rules="[rules.required, rules.email]"
             style="margin-top: 1vw;"
-            type="email"
-            validate-on="input"
-            variant="outlined"
-            width="27vw"
           />
 
           <v-text-field
             v-model="password"
-            clearable
-            color="#d28d8d"
+            validate-on="input"
             label="Contraseña"
+            clearable
+            type="password"
+            variant="outlined"
+            color="#d28d8d"
+            width="27vw"
             :rules="[rules.required, rules.password]"
             style="margin-top: 1vw;"
-            type="password"
-            validate-on="input"
-            variant="outlined"
-            width="27vw"
-          />          
-          
+          />
+
           <v-btn
             color="#d28d8d"
             rounded="lg"
-            style="align-self: center; margin-top: 2.3vh; margin-bottom: 1vw"
             width="12vw"
             @click="validateForm"
             :loading="loading"
+            style="align-self: center; margin-top: 2.3vh; margin-bottom: 1vw"
           >
             Continuar
           </v-btn>
 
           <v-btn
             color="#90979a"
+            variant="text"
             size="small"
             style="align-self: center;"
             :to="'/'"
-            variant="text"
           > Volver al inicio </v-btn>
         </v-form>
       </v-col>
@@ -105,14 +107,14 @@
 </template>
 
 <script setup>
-  import { computed, ref } from 'vue';
-  import { User } from '@/api/user.js';
-  import { useUserStore } from '@/stores/userStore.js';
-  import { useRouter } from 'vue-router';
-  import LoginHeader from '../components/LoginHeader.vue';
+import { ref, computed } from 'vue';
+import { User } from '@/api/user.js';
+import { useUserStore } from '@/stores/userStore.js';
+import { useRouter } from 'vue-router';
+import LoginHeader from '../components/LoginHeader.vue';
 
-  const userStore = useUserStore();
-  const router = useRouter();
+const userStore = useUserStore();
+const router = useRouter();
 
 const form = ref(null);
 const firstName = ref('');
@@ -135,20 +137,25 @@ const rules = {
     const hasNumber = /[0-9]/.test(value);
     const hasMinLength = value?.length >= 8;
 
-      if(!hasUpperCase) return 'La contraseña debe tener al menos una mayúscula';
-      if(!hasNumber) return 'La contraseña debe tener al menos un número';
-      if(!hasMinLength) return 'La contraseña debe tener al menos 8 caracteres';
+    if(!hasUpperCase) return 'La contraseña debe tener al menos una mayúscula';
+    if(!hasNumber) return 'La contraseña debe tener al menos un número';
+    if(!hasMinLength) return 'La contraseña debe tener al menos 8 caracteres';
 
-      return true;
-    },
-    date: value => {
-      const pattern = /^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0-2])\/\d{4}$/;
-      return pattern.test(value) || 'Formato de fecha inválido (DD/MM/YYYY)';
-    },
+    return true;
+  },
+  date: value => {
+    const pattern = /^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0-2])\/\d{4}$/;
+    return pattern.test(value) || 'Formato de fecha inválido (DD/MM/YYYY)';
   }
+}
 
-  function formatDate (value) {
-    const digits = value.replace(/\D/g, '').slice(0, 8)
+const isFormValid = computed(() => {
+  return rules.email(email.value) === true &&
+    rules.password(password.value) === true &&
+    rules.date(birthDate.value) === true &&
+    firstName.value.length > 0 &&
+    lastName.value.length > 0;
+});
 
 async function load(user) {
   loading.value = true
@@ -177,33 +184,6 @@ const validateForm = async () => {
   load(user);
 };
 
-  const isFormValid = computed(() => {
-    return rules.email(email.value) === true &&
-      rules.password(password.value) === true &&
-      rules.date(birthDate.value) === true &&
-      firstName.value.length > 0 &&
-      lastName.value.length > 0;
-  });
-
-  const validateForm = async () => {
-    const { valid } = await form.value.validate();
-    if (!valid) {
-      return false;
-    }
-
-    const [day, month, year] = birthDate.value.split('/');
-    const formattedDate = `${year}-${month}-${day}`;
-
-    const user = new User(firstName.value, lastName.value, formattedDate, email.value, password.value);
-    try {
-      await userStore.register(user);
-      userStore.email = email.value
-      router.push('/verificar');
-    } catch (error) {
-      showAlert.value = true;
-      alertMessage.value = 'Este correo ya esta en uso.';
-    }
-  };
 
 
 </script>
